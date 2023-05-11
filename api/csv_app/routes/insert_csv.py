08578c3635cb868ebca7b654fcbd99dd2621c72c
Gabriel Bossardi <contato@gabrielbossardi.com.br>
2023-05-10 23:35:19 -0300

    Create file_handler to find filepaths

--- api/csv_app/routes/insert_csv.py
+++ api/csv_app/routes/insert_csv.py
@@ -1,7 +1,11 @@
 from csv_app import db
 from csv_app.routes import bp
+from csv_app.utils.file_handler import get_file_paths
 
 
 @bp.route('/insert_csv', methods=['GET'])
 def insert_csv():
-    return 'This is the insert CSV route'
+    valid_files = ['departments', 'hired_employees', 'jobs']
+    file_paths = get_file_paths(valid_files)
+
+    return f'These are the files: {file_paths}'
---
+++ api/csv_app/utils/file_handler.py
@@ -0,0 +1,14 @@
+import os
+
+CSV_UPLOAD_DIR = os.environ.get('CSV_UPLOAD_DIR')
+
+def get_file_paths(valid_files):
+    file_paths = {}
+    for root_folder, subfolders, files in os.walk(CSV_UPLOAD_DIR):
+        for file in files:
+            file = os.path.splitext(file)[0]
+            if file in valid_files:
+                file_path = os.path.join(root_folder, file)
+                file_paths[file] = file_path
+
+    return file_paths
