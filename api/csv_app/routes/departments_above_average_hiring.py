c585e96d816b4da0b5ac315d454f02cdfbed864f
Gabriel Bossardi <contato@gabrielbossardi.com.br>
2023-05-11 14:13:16 -0300

    Apply try exception over routes

--- api/csv_app/routes/departments_above_average_hiring.py
+++ api/csv_app/routes/departments_above_average_hiring.py
@@ -9,37 +9,44 @@ from sqlalchemy.types import Date
 
 @bp.route("/departments_above_average_hiring", methods=["GET"])
 def departments_above_average_hiring():
-    hiring_by_department = db.session.query(
-        Department.id,
-        Department.department,
-        func.count().label('hired')
-    ).join(Employee, Department.id == Employee.department_id).filter(
-        func.date_trunc('year', cast(Employee.datetime, Date)) == '2021-01-01'
-    ).group_by(Department.id, Department.department).subquery()
+    try:
+        hiring_by_department = db.session.query(
+            Department.id,
+            Department.department,
+            func.count().label('hired')
+        ).join(Employee, Department.id == Employee.department_id).filter(
+            func.date_trunc(
+                'year', cast(Employee.datetime, Date)
+            ) == '2021-01-01'
+        ).group_by(Department.id, Department.department).subquery()
 
-    final = db.session.query(
-        hiring_by_department.c.id,
-        hiring_by_department.c.department,
-        hiring_by_department.c.hired,
-        func.avg(hiring_by_department.c.hired).over()
-            .label('avg_employees_hired')
-    ).select_from(hiring_by_department).subquery()
+        final = db.session.query(
+            hiring_by_department.c.id,
+            hiring_by_department.c.department,
+            hiring_by_department.c.hired,
+            func.avg(hiring_by_department.c.hired).over()
+                .label('avg_employees_hired')
+        ).select_from(hiring_by_department).subquery()
 
-    results = db.session.query(
-        final.c.id,
-        final.c.department,
-        final.c.hired
-    ).filter(final.c.hired > final.c.avg_employees_hired).order_by(
-        final.c.hired.desc()
-    ).all()
+        results = db.session.query(
+            final.c.id,
+            final.c.department,
+            final.c.hired
+        ).filter(final.c.hired > final.c.avg_employees_hired).order_by(
+            final.c.hired.desc()
+        ).all()
 
-    output = []
-    for row in results:
-        id, department, hired = row
-        output.append({
-            'id': id,
-            'department': department,
-            'hired': hired
-        })
+        output = []
+        for row in results:
+            id, department, hired = row
+            output.append({
+                'id': id,
+                'department': department,
+                'hired': hired
+            })
+    except Exception as e:
+        return jsonify(
+            {'message': f'{e}'}
+        ), 400
 
     return jsonify(output), 200
--- api/csv_app/routes/employee_quarter_stats.py
+++ api/csv_app/routes/employee_quarter_stats.py
@@ -10,76 +10,81 @@ from sqlalchemy.types import Date
 
 @bp.route("/employee_quarter_stats", methods=["GET"])
 def employee_quarter_stats():
-    values_by_quarter = (
-        db.session.query(
-            Department.department,
-            Job.job,
-            func.extract("quarter", cast(Employee.datetime, Date))
-                .label("quarter"),
-            func.count().label("employees_hired"),
+    try:
+        values_by_quarter = (
+            db.session.query(
+                Department.department,
+                Job.job,
+                func.extract("quarter", cast(Employee.datetime, Date))
+                    .label("quarter"),
+                func.count().label("employees_hired"),
+            )
+            .join(Department, Employee.department_id == Department.id)
+            .join(Job, Employee.job_id == Job.id)
+            .filter(
+                func.date_trunc(
+                    "year",
+                    cast(Employee.datetime, Date)
+                ) == "2021-01-01"
+            )
+            .group_by(
+                Department.department,
+                Job.job,
+                func.extract("quarter", cast(Employee.datetime, Date)),
+            )
+            .subquery()
         )
-        .join(Department, Employee.department_id == Department.id)
-        .join(Job, Employee.job_id == Job.id)
-        .filter(
-            func.date_trunc(
-                "year",
-                cast(Employee.datetime, Date)
-            ) == "2021-01-01"
-        )
-        .group_by(
-            Department.department,
-            Job.job,
-            func.extract("quarter", cast(Employee.datetime, Date)),
-        )
-        .subquery()
-    )
 
-    final = (
-        db.session.query(
-            values_by_quarter.c.department,
-            values_by_quarter.c.job,
-            func.sum(values_by_quarter.c.employees_hired)
-            .filter(values_by_quarter.c.quarter == 1)
-            .label("q1"),
-            func.sum(values_by_quarter.c.employees_hired)
-            .filter(values_by_quarter.c.quarter == 2)
-            .label("q2"),
-            func.sum(values_by_quarter.c.employees_hired)
-            .filter(values_by_quarter.c.quarter == 3)
-            .label("q3"),
-            func.sum(values_by_quarter.c.employees_hired)
-            .filter(values_by_quarter.c.quarter == 4)
-            .label("q4"),
+        final = (
+            db.session.query(
+                values_by_quarter.c.department,
+                values_by_quarter.c.job,
+                func.sum(values_by_quarter.c.employees_hired)
+                .filter(values_by_quarter.c.quarter == 1)
+                .label("q1"),
+                func.sum(values_by_quarter.c.employees_hired)
+                .filter(values_by_quarter.c.quarter == 2)
+                .label("q2"),
+                func.sum(values_by_quarter.c.employees_hired)
+                .filter(values_by_quarter.c.quarter == 3)
+                .label("q3"),
+                func.sum(values_by_quarter.c.employees_hired)
+                .filter(values_by_quarter.c.quarter == 4)
+                .label("q4"),
+            )
+            .group_by(values_by_quarter.c.department, values_by_quarter.c.job)
+            .subquery()
         )
-        .group_by(values_by_quarter.c.department, values_by_quarter.c.job)
-        .subquery()
-    )
 
-    results = (
-        db.session.query(
-            final.c.department,
-            final.c.job,
-            func.coalesce(final.c.q1, 0).label("q1"),
-            func.coalesce(final.c.q2, 0).label("q2"),
-            func.coalesce(final.c.q3, 0).label("q3"),
-            func.coalesce(final.c.q4, 0).label("q4"),
+        results = (
+            db.session.query(
+                final.c.department,
+                final.c.job,
+                func.coalesce(final.c.q1, 0).label("q1"),
+                func.coalesce(final.c.q2, 0).label("q2"),
+                func.coalesce(final.c.q3, 0).label("q3"),
+                func.coalesce(final.c.q4, 0).label("q4"),
+            )
+            .order_by(final.c.department, final.c.job)
+            .all()
         )
-        .order_by(final.c.department, final.c.job)
-        .all()
-    )
 
-    output = []
-    for row in results:
-        department, job, q1, q2, q3, q4 = row
-        output.append(
-            {
-                "department": department,
-                "job": job,
-                "q1": q1,
-                "q2": q2,
-                "q3": q3,
-                "q4": q4,
-            }
-        )
+        output = []
+        for row in results:
+            department, job, q1, q2, q3, q4 = row
+            output.append(
+                {
+                    "department": department,
+                    "job": job,
+                    "q1": q1,
+                    "q2": q2,
+                    "q3": q3,
+                    "q4": q4,
+                }
+            )
+    except Exception as e:
+        return jsonify(
+            {'message': f'{e}'}
+        ), 400
 
     return jsonify(output), 200
